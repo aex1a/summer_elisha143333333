9976ff7d53e60df039b01778a7962a8af4549ada
aex1a <101616036+aex1a@users.noreply.github.com>
2022-03-17 17:16:16 +0800

    files added

---
+++ script.js
@@ -0,0 +1,20 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+let bg = document.getElementById("bg");
+                let moon = document.getElementById("moon");
+                let mountain = document.getElementById("mountain");
+                let house = document.getElementById("house");
+                let text = document.getElementById("text");
+        
+                window.addEventListener('scroll', function(){
+                    var value = window.scrollY;
+        
+                    bg.style.top = value * 0.1 + 'px';
+                    moon.style.left = value * +1 + 'px';
+                    mountain.style.top = -value * -0.15 + 'px';
+                    house.style.top = value * 0.15 + 'px';
+                    text.style.top = value * 0.99+ 'px';
+                })
+
+  .fromTo(".content-text", { opacity: 0 }, { opacity: 1, duration: 3 });
